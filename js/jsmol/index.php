3bb596872baecbf6878a839247b92c3304263b04
ro <ro@dlab.cl>
2015-06-17 17:43:27 -0300

    - detalles jsmol -  index al peo - enchular pyhlogeny

--- js/jsmol/index.php
+++ js/jsmol/index.php
@@ -7,26 +7,38 @@
 jmolInitialize(".")
 
 function doLoad() {
- document.getElementById("info").reset()
+// document.getElementById("info").reset();
+setTimeout(function(){ document.getElementById("warning").hidden=true; }, 2000);
 }
 
 </script>
 </head>
 <body onload = doLoad()>
 
-<table border = 1 cellpadding=40>
-<tr>
-
-<td id=tdA> 
-<script>jmolApplet(400,"load ../../proteinModels/<?php echo $_GET['pdb']?>;cartoon only;color cartoon structure;");</script>
+<!--<table border = 1 cellpadding=40>-->
+<!--<tr>-->
+<!--<td id=tdA>-->
+<script>
+    var pdb="<?php echo $_GET['pdb']?>",surface="";
+    if((pdb+"").includes("lipids")||pdb.includes("solvate"))
+    {
+        surface="isosurface vdw translucent";
+//        document.writeln("This could take some minute to load and may crush your browser.");
+//        document.getElementById("warning").hidden=false;
+    }
+//    alert("XD pdb:"+pdb);
+    jmolApplet(400,"load ../../proteinModels/"+pdb+";set antialiasDisplay false;set platformSpeed 1;cartoon only;color cartoon structure;"+surface);
+</script>
+<div id='warning' ><img src="../../css/images/ajax-loader.gif" alt="Searching" />This could take some minute to load and may freeze your browser. Please wait.</div>
 <!--<script>jmolApplet(400,"load ../../proteinModels/cx39/AA_HC_CX39.pdb;cartoon only;color cartoon structure;");</script>-->
-</td>
-</tr>
+<!--</td>-->
+<!--</tr>-->
 
-</table>
-<form id=info>
- <a href=javascript:syncAll()>reset</a>
-</form>
+<!--</table>-->
+<!--<form id=info>-->
+<!-- <a href=javascript:doLoad()>reset</a>-->
+ 
+<!--</form>-->
 
 </body>
 </html>
